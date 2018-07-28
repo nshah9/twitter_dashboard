90d8a21e5cdd57f1e97ab10b0f0239e9632a4e11
Nishit Shah <nishitshah@Nishits-MacBook-Pro.local>
2018-07-29 00:56:01 +0530

    Show tweet along with engagement metric

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,9 +1,9 @@
 class SearchesController < ApplicationController
   def show
-    client = Twitter::REST::Client.new do |config|
+    @client = Twitter::REST::Client.new do |config|
       config.consumer_key        = "36LXFREo2t29U54LGkw"
       config.consumer_secret     = "WuOWDFdG2AB6nMFSv0rKKui0XtR0yAK1Gg089XG9EM"
     end
-    @tweets = client.search("@#{params[:search][:user_handle]}", result_type: 'popular')
+    @tweets = @client.search("@#{params[:search][:user_handle]}", result_type: 'popular')
   end
 end
