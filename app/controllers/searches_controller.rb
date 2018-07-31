49382636076426f2f36e0c6384f5f1b6f3f7b03d
Nishit Shah <nishitshah@Nishits-MacBook-Pro.local>
2018-07-31 10:56:06 +0530

    Fetch followers count to calculate engagement metric

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,6 +4,8 @@ class SearchesController < ApplicationController
       config.consumer_key        = "36LXFREo2t29U54LGkw"
       config.consumer_secret     = "WuOWDFdG2AB6nMFSv0rKKui0XtR0yAK1Gg089XG9EM"
     end
-    @tweets = @client.search("@#{params[:search][:user_handle]}", result_type: 'popular')
+    search_user_handle = "@#{params[:search][:user_handle]}"
+    @followers_count = @client.user(search_user_handle).followers_count
+    @tweets = @client.search(search_user_handle, result_type: 'popular')
   end
 end
